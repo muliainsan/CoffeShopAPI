b8de7283cdc16869f40db90c056b1e312df7e6fa
isa <isa.insan@xtremax.com>
2020-09-22 22:08:15 +0700

    Add menus

---
+++ Models/Request/AddMenuRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeShop.Models.Request
+{
+    public class AddMenuRequest
+    {
+        public string MenuName { get; set; }
+        public double Price { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
