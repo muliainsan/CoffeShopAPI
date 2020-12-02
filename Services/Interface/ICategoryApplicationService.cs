d570993fab49e877304bada0a10368ab75e8b27f
Isa Mulia Insan <insanmuliaisa@gmail.com>
2020-12-02 11:09:14 +0700

    add category interface

---
+++ Services/Interface/ICategoryApplicationService.cs
@@ -0,0 +1,22 @@
+﻿using CoffeShop.Enties;
+using CoffeShop.EntitiesFramework;
+using CoffeShop.Models;
+using CoffeShop.Models.Request;
+using CoffeShop.Models.Request.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeShop.Services.Interface
+{
+    public interface ICategoryApplicationService
+    {
+
+        Task<Response<Category>> AddCategory(AddCategoryRequest request);
+        ListResponse<Category> GetCategory(GetCategoryRequest request);
+        Response<Category> GetDetailCategory(IdOnlyRequest request);
+        Response<Category> UpdateCategory(UpdateCategoryRequest request);
+        Response<Category> DeleteCategory(IdOnlyRequest request);
+    }
+}
